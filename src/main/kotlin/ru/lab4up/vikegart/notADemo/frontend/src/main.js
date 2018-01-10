dd1fb9c7c0eedf40a8d23fda2034e817d9a7879c
vikegart <vikegart@gmail.com>
2018-01-10 23:32:57 +0400

    интегрировал старый проект

--- src/main/kotlin/ru/lab4up/vikegart/notADemo/frontend/src/main.js
+++ src/main/kotlin/ru/lab4up/vikegart/notADemo/frontend/src/main.js
@@ -1,21 +1,52 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-
+import App from './components/App.vue'
+import VueRouter from 'vue-router'
+import store from './store'
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.css'
+import './stylus/main.styl'
 import VueResource from 'vue-resource'
 
-import App from './App'
+import FindEvent        from './components/pages/FindEvent.vue'
+import FavoriteEvents   from './components/pages/FavoriteEvents.vue'
+import HistoryEvents    from './components/pages/HistoryEvents.vue'
+import CreateEvent      from './components/pages/CreateEvent.vue'
+import MyEvent          from './components/pages/MyEvent.vue'
+import Event            from './components/pages/Event.vue'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
+Vue.use(VueRouter);
 Vue.use(VueResource);
 
-Vue.config.productionTip = false
+let router = new VueRouter({
+    routes: [
+        {
+            name: 'find',
+            path: '/',
+            components: { default: FindEvent }
+        }, {
+            name: 'create',
+            path: '/create',
+            components: { default: CreateEvent }
+        }, {
+            name: 'event',
+            path: '/event/:id',
+            components: { default: Event}
+        }, {
+            name: 'favorites',
+            path: '/favorite',
+            components: { default: FavoriteEvents }
+        }, {
+            name: 'history',
+            path: '/history',
+            components: { default: HistoryEvents }
+        }
 
-/* eslint-disable no-new */
+    ]
+});
+
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+    el: '#app',
+    router: router,
+    store: store,
+    render: h => h(App)
+});
